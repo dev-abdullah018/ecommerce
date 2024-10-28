7a89b32f803bef6ef6966a298b6141e4ebb7200e
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2024-10-28 14:19:59 +0600

    response-module-create

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const adminModel = require("../models/adminModel")
+const { responseReturn } = require("../utils/response")
 
 const authController = async (req,res) =>{
    const {email, password} = req.body
@@ -9,10 +10,10 @@ const authController = async (req,res) =>{
        if(admin){
 
        }else{
-        
+        responseReturn(res, 404, {error: "Email not found!"})
        }
    } catch (error) {
-    
+     responseReturn(res, 500, {error: error.message})
    }
 }
 
