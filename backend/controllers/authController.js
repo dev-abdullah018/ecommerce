1336a6f0554c9bcb2d490f6ee882060e68570ec0
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2024-10-28 14:49:48 +0600

    match-password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
 const adminModel = require("../models/adminModel")
 const { responseReturn } = require("../utils/response")
+const bcrypt = require('bcrypt');
+const { createToken } = require("../utils/tokenCreate");
 
 const authController = async (req,res) =>{
    const {email, password} = req.body
@@ -8,6 +10,19 @@ const authController = async (req,res) =>{
        const admin = await adminModel.findOne({email: email})
        
        if(admin){
+         const matchPassword = await bcrypt.compare(password, admin.password)
+         console.log(matchPassword);
+
+         if(matchPassword){
+           const token = await createToken({
+            id: admin._id,
+            role: admin.role
+           })
+           console.log("token" , token);
+           
+         }else{
+            responseReturn(res, 404, {error: "Password not matched!"})
+         }
          
        }else{
         responseReturn(res, 404, {error: "Email not found!"})
